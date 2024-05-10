72c4975f6b277100674f13c766719f65834a844e
Carlos Martinez <ced.mendoza@gmail.com>
2024-05-10 20:46:32 +0000

    database: Add fetch_first and execute methods

--- dependencies/database.py
+++ dependencies/database.py
@@ -1,23 +1,20 @@
 import logging
 
 import asyncpg
-from fastapi import HTTPException
+from fastapi import HTTPException, FastAPI, Request
 
 from settings import settings
 
-pool: asyncpg.Pool | None = None
 logger: logging.Logger = logging.getLogger(__name__)
 
 
 class Session:
-    cursor_ctx: asyncpg.Pool | None = None
-
-    def __init__(self, cursor_ctx):
-        self.cursor_ctx = cursor_ctx
+    def __init__(self, pool):
+        self.pool = pool
 
     async def fetch_rows(self, query: str, *args) -> list:
         try:
-            async with self.cursor_ctx as cursor:
+            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
                 if args:
                     data = await cursor.fetch(query, *args)
                 else:
@@ -28,11 +25,25 @@ class Session:
             logger.error(f"exception '{e}' executing query: `{query}`")
             raise HTTPException(status_code=500, detail="internal error, try later")
 
+    async def fetch_first(self, query: str, *args) -> asyncpg.Record:
+        rows = await self.fetch_rows(query, *args)
+        return rows[0] if len(rows) > 0 else None
 
-async def setup_pool():
-    global pool
-    logger.debug(f"setting up db pool max={settings.database_pool_max_size} min={settings.database_pool_min_size}")
-    pool = await asyncpg.create_pool(
+    async def execute(self, query: str, *args) -> str:
+        try:
+            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
+                if args:
+                    status = await cursor.execute(query, *args)
+                else:
+                    status = await cursor.execute(query)
+                return status
+        except Exception as e:
+            logger.error(f"exception '{e}' executing query: `{query}`")
+            raise HTTPException(status_code=500, detail="internal error, try later")
+
+
+async def get_connection_pool() -> asyncpg.Pool:
+    return await asyncpg.create_pool(
         host=settings.database_host,
         port=settings.database_port,
         database=settings.database_name,
@@ -43,13 +54,15 @@ async def setup_pool():
     )
 
 
-async def close_pool():
-    global pool
+async def setup_pool(app: FastAPI):
+    logger.debug(f"setting up db pool max={settings.database_pool_max_size} min={settings.database_pool_min_size}")
+    app.state.pool = await get_connection_pool()
+
+
+async def close_pool(app: FastAPI):
     logger.debug("closing db pool")
-    await pool.close()
+    await app.state.pool.close()
 
 
-async def get_session() -> Session:
-    global pool
-    return Session(pool.acquire(timeout=settings.database_timeout))
-
+async def get_session(request: Request) -> Session:
+    return Session(request.app.state.pool)
