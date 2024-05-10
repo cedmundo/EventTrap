4a05b4570a0525b2f7249ff0cc78873cbd9ec415
Carlos Martinez <ced.mendoza@gmail.com>
2024-05-10 20:48:44 +0000

    search: Add search nearby events with query params

--- routers/search.py
+++ routers/search.py
@@ -1,14 +1,28 @@
 from typing import Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 
 from repositories.events import EventsRepository
 
 router = APIRouter()
+coordinate_regex = r'^((\-?|\+?)?\d+(\.\d+)?),\s*((\-?|\+?)?\d+(\.\d+)?)$'
 
 
-@router.get("/search")
-async def search(events: Annotated[EventsRepository, Depends(EventsRepository)],
-                 location: str, radius: float, skip: int = 0, limit: int = 100):
-    return await events.search_events(location, radius, skip, limit)
+class SearchQueryParams:
+    def __init__(
+            self,
+            location: Annotated[str, Query(pattern=coordinate_regex, description="standard lat,long coordinate")],
+            radius: Annotated[float, Query(ge=100.0, lt=10000.0, description="radius in meters")],
+            skip: Annotated[int, Query(ge=0, description="result skip for pagination")] = 0,
+            limit: Annotated[int, Query(ge=10, lt=100, description="amount of results per page")] = 50,
+    ):
+        self.location = location
+        self.radius = radius
+        self.skip = skip
+        self.limit = limit
 
+
+@router.get("/search-nearby", tags=["events"], description="Search events in geographic area")
+async def search_nearby(events: Annotated[EventsRepository, Depends()],
+                        params: Annotated[SearchQueryParams, Depends()]):
+    return await events.search_events(params.location, params.radius, params.skip, params.limit)
