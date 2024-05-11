7b48991457b64476f89dff8d4f3ff98724ecae40
Carlos Martinez <ced.mendoza@gmail.com>
2024-05-10 19:24:08 -0600

    refactor: Improve database connections to support test and mocking.

--- dependencies/database.py
+++ dependencies/database.py
@@ -1,6 +1,10 @@
+import asyncio
 import logging
+import uuid
+from typing import AsyncGenerator
 
 import asyncpg
+import pytest_asyncio
 from fastapi import HTTPException, FastAPI, Request
 
 from settings import settings
@@ -11,16 +15,37 @@ logger: logging.Logger = logging.getLogger(__name__)
 class Session:
     def __init__(self, pool):
         self.pool = pool
+        self.connection = None
+        self.connection_id = None
+
+    async def setup_testing_connection(self, connection_id=None):
+        self.connection = await self.pool.acquire(timeout=settings.database_timeout)
+        # self.connection_id = str(uuid.uuid4()) if connection_id is None else connection_id
+        # await self.connection.execute("PREPARE TRANSACTION '%s'" % self.connection_id)
+
+    async def close_testing_connection(self):
+        # await self.connection.execute("ROLLBACK PREPARED '%s'" % self.connection_id)
+        if self.connection:
+            await self.pool.release(self.connection)
 
     async def fetch_rows(self, query: str, *args) -> list:
         try:
-            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
-                if args:
-                    data = await cursor.fetch(query, *args)
-                else:
-                    data = await cursor.fetch(query)
+            if self.connection is not None:
+                return await self.connection.fetch(query, *args)
 
-            return data
+            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
+                return await cursor.fetch(query, *args)
+        except Exception as e:
+            logger.error(f"exception '{e}' executing query: `{query}`")
+            raise HTTPException(status_code=500, detail="internal error, try later")
+
+    async def execute(self, query: str, *args) -> str:
+        try:
+            if self.connection is not None:
+                return await self.connection.execute(query, *args)
+
+            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
+                return await cursor.execute(query, *args)
         except Exception as e:
             logger.error(f"exception '{e}' executing query: `{query}`")
             raise HTTPException(status_code=500, detail="internal error, try later")
@@ -29,28 +54,17 @@ class Session:
         rows = await self.fetch_rows(query, *args)
         return rows[0] if len(rows) > 0 else None
 
-    async def execute(self, query: str, *args) -> str:
-        try:
-            async with self.pool.acquire(timeout=settings.database_timeout) as cursor:
-                if args:
-                    status = await cursor.execute(query, *args)
-                else:
-                    status = await cursor.execute(query)
-                return status
-        except Exception as e:
-            logger.error(f"exception '{e}' executing query: `{query}`")
-            raise HTTPException(status_code=500, detail="internal error, try later")
 
-
-async def get_connection_pool() -> asyncpg.Pool:
+async def get_connection_pool(loop=None, force_size=None) -> asyncpg.Pool:
     return await asyncpg.create_pool(
         host=settings.database_host,
         port=settings.database_port,
         database=settings.database_name,
         user=settings.database_username,
         password=settings.database_password,
-        max_size=settings.database_pool_max_size,
-        min_size=settings.database_pool_min_size,
+        max_size=settings.database_pool_max_size if force_size is None else force_size,
+        min_size=settings.database_pool_min_size if force_size is None else force_size,
+        loop=loop,
     )
 
 
@@ -66,3 +80,13 @@ async def close_pool(app: FastAPI):
 
 async def get_session(request: Request) -> Session:
     return Session(request.app.state.pool)
+
+
+@pytest_asyncio.fixture
+async def test_db() -> AsyncGenerator[Session, None]:
+    pool = await get_connection_pool(loop=asyncio.get_running_loop(), force_size=1)
+    session = Session(pool)
+    await session.setup_testing_connection()
+    yield session
+    await session.close_testing_connection()
+    await pool.close()
